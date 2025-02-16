8fcf87050155dc817dcce7e65f8710d0f8dc1a10
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-02-16 17:49:23 +0300

    Написал Expression

--- src/rules/Expression.h
+++ src/rules/Expression.h
@@ -1,8 +1,37 @@
 #pragma once
-#include "../Reader.h"
+#include "Operations.h"
+#include "SimExp.h"
 #include "../lexer/Lexer.h"
 
+/**
+ * expressionRem -> e | relOp simexp expressionRem
+ */
+inline bool ExpressionRem(Lexer& lexer)
+{
+	if (lexer.Empty())
+	{
+		return true;
+	}
+
+	if (IsRelOp(lexer.Peek()))
+	{
+		lexer.Get();
+		return SimExp(lexer) && ExpressionRem(lexer);
+	}
+
+	return true;
+}
+
+/**
+ * expression -> simexp expressionRem
+ * expressionRem -> e | relOp simexp expressionRem
+ */
 inline bool Expression(Lexer& lexer)
 {
-	return false;
+	if (lexer.Empty())
+	{
+		return false;
+	}
+
+	return SimExp(lexer) && ExpressionRem(lexer);
 }
