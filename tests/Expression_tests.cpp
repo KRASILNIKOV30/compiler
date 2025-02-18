4b1715cf9ca8ae36fb9fc7401f1463aabf0772f4
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-02-18 11:07:38 +0300

    Добавил тесты

--- tests/Expression_tests.cpp
+++ tests/Expression_tests.cpp
@@ -23,6 +23,14 @@ TEST_CASE("positive tests")
 	Check("5 -(-5 - -5)");
 	Check("-a + 5.3E-15 * -(-a + -b * (a * -b) -c) != abc");
 	Check("!a[7][a+5][b(3.5, c.d[f * ab])] OR 15 * (r - br MOD 5) AND TRUE");
+	Check("(a) <= (-5) != not - 5");
+	Check("(a) <= -(-5 != not - 5)");
+	Check("+(+a) <= -(-+5 != not - 5)");
+	Check("(+(+a) <= -(-+5 != not - 5)) * (a and 0.5E-3) or 1.0");
+	Check("(a) +(a)+ not - not a");
+	Check("(((a) +(a))+ not - (not a))");
+	Check("0.5E-20");
+	Check("+not-not-+not-(-not-a)");
 }
 
 TEST_CASE("negative tests")
@@ -38,4 +46,12 @@ TEST_CASE("negative tests")
 	CheckFalse(".");
 	CheckFalse("-a + 5.3E-15 * (-a + -b * (a * -b) -c != abc");
 	CheckFalse("!a[7][a+5][b(3.5, c.d[f * ab,])] OR 15 * (r - br MOD 5) AND TRUE");
+	CheckFalse("(a) <= (-5 != not - 5");
+	CheckFalse("(a)- <= -(-5 != not - 5)");
+	CheckFalse("(+(+a <= -(-+5 != not - 5)) * (a and 0.5E-3) or 1.0");
+	CheckFalse("(a) +(a)+ or - not a");
+	CheckFalse("05E-20");
+	CheckFalse("05.E-20");
+	CheckFalse("15.E-20");
+	CheckFalse("(((a) +(a)) ! not - (not a))");
 }
