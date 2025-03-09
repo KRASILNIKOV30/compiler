64e46c36cd0d113e82cc743536363419a06708be
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-03-09 19:50:19 +0300

    Добавил тест

--- tests/LLParser/LLParser_tests.cpp
+++ tests/LLParser/LLParser_tests.cpp
@@ -17,17 +17,25 @@ TEST_CASE("ll-parser tests")
 
 	CHECK(parser.Parse("func(id1, id2)"));
 	CHECK(parser.Parse("object.create(id)"));
+	CHECK(parser.Parse("2 func(id1, id2)"));
+
 	CHECK_FALSE(parser.Parse("object.create(id"));
 	CHECK(parser.GetError() == ErrorReason{
 		.expected = { ")", "," },
 		.received = Token{TokenType::ERROR, "", 16, Error::EMPTY_INPUT}});
-	CHECK(parser.Parse("2 func(id1, id2)"));
+
 	CHECK_FALSE(parser.Parse("2.2 func(id1, id2)"));
 	CHECK(parser.GetError() == ErrorReason{
 		.expected = { "id", "int" },
 		.received = Token{TokenType::FLOAT, "2.2", 0, Error::NONE}});
+
 	CHECK_FALSE(parser.Parse("2 id func(id1, id2)"));
 	CHECK(parser.GetError() == ErrorReason{
 		.expected = { "(" },
 		.received = Token{TokenType::ID, "func", 5, Error::NONE}});
+
+	CHECK_FALSE(parser.Parse("func(id1, id2) 2"));
+	CHECK(parser.GetError() == ErrorReason{
+		.expected = { "#" },
+		.received = Token{TokenType::INTEGER, "2", 15, Error::NONE}});
 }
