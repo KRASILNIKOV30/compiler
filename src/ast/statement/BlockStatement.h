30304e238033382688501e180e83bf270ddf3c65
Bogdan Krasilnikov <bogdan.krasilnikov@ispring.com>
2025-06-03 18:28:02 +0300

    Унаследовал Program от BlockStatement

--- src/ast/statement/BlockStatement.h
+++ src/ast/statement/BlockStatement.h
@@ -4,26 +4,40 @@
 #include <variant>
 #include <vector>
 
-struct BlockStatement : Statement
+using ProgramNode = std::variant<DeclarationPtr, StatementPtr>;
+
+class BlockStatement : public Statement
 {
+public:
 	void Generate(CodeGenerator& generator) const override
 	{
-		for (const auto& bodyPart : body)
+		for (const auto& bodyPart : m_body)
 		{
 			std::visit([&](const auto& bodyNode) { Generate(generator, bodyNode); }, bodyPart);
 		}
 	}
 
-	std::vector<std::variant<Declaration, Statement>> body;
+	void Add(DeclarationPtr&& decl)
+	{
+		m_body.emplace_back(std::move(decl));
+	}
+
+	void Add(StatementPtr&& statement)
+	{
+		m_body.emplace_back(std::move(statement));
+	}
 
 private:
-	static void Generate(CodeGenerator& generator, const Declaration& declaration)
+	static void Generate(CodeGenerator& generator, DeclarationPtr const& declaration)
 	{
-		declaration.Generate(generator);
+		declaration->Generate(generator);
 	}
 
-	static void Generate(CodeGenerator& generator, const Statement& statement)
+	static void Generate(CodeGenerator& generator, StatementPtr const& statement)
 	{
-		statement.Generate(generator);
+		statement->Generate(generator);
 	}
+
+private:
+	std::vector<ProgramNode> m_body{};
 };
