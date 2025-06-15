5e7ce1d69e82104a640c0ba667bb00149167b170
MrKoT321 <vladimir.fedoseev.2004@mail.ru>
2025-06-15 19:40:58 +0300

    Добавил поддержку зарезервированных функций

--- src/ast/expression/CallExpression.h
+++ src/ast/expression/CallExpression.h
@@ -2,10 +2,10 @@
 #include "Expression.h"
 #include <vector>
 
-struct CallExpression : Expression
+class CallExpression : Expression
 {
 public:
-	explicit CallExpression(std::string callee, Type const& type, std::vector<Expression> arguments)
+	explicit CallExpression(std::string const& callee, Type const& type, std::vector<ExpressionPtr> const& arguments)
 		: Expression(type)
 		, m_callee(callee)
 		, m_arguments(arguments)
@@ -15,15 +15,33 @@ public:
 	void Generate(CodeGenerator& generator) const override {
 		for (auto const& argument : m_arguments)
 		{
-			argument.Generate(generator);
+			argument->Generate(generator);
 		}
 
-		auto functionPos = generator.GetConstantPosOrAdd({ PrimitiveType::STRING }, m_callee);
-		generator.AddInstruction("get_global " + functionPos);
-		generator.AddInstruction("call");
+		if (NATIVE_FUNCTIONS_WITH_ARGUMENTS_COUNT.contains(m_callee))
+		{
+			if (m_arguments.size() != NATIVE_FUNCTIONS_WITH_ARGUMENTS_COUNT.at(m_callee))
+			{
+				throw std::runtime_error("Invalid count of arguments of " + m_callee);
+			}
+			const auto functionPos = generator.GetConstantPosOrAdd({ PrimitiveType::STRING }, m_callee);
+			generator.AddInstruction("get_global " + functionPos);
+			generator.AddInstruction("call");
+		}
+		else
+		{
+			// TODO: доделать вместе с функциями
+		}
 	};
 
 private:
 	std::string m_callee;
-	std::vector<Expression> m_arguments;
+	std::vector<ExpressionPtr> m_arguments;
+
+	static inline const std::unordered_map<std::string, int> NATIVE_FUNCTIONS_WITH_ARGUMENTS_COUNT = {
+		{"print", 1},
+		{"println", 1},
+		{"getTimestamp", 0},
+		{"sqrt", 1},
+	};
 };
