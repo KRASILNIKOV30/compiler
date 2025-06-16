718a1b11065174d61e7ea61a76eba3975276b850
Vladimir Fedoseev <vladimir.fedoseev@ispring.com>
2025-06-16 19:09:36 +0300

    Добавил кодогенерацию для массива

--- src/ast/expression/ArrayExpression.h
+++ src/ast/expression/ArrayExpression.h
@@ -1,8 +1,20 @@
 #pragma once
 #include "Expression.h"
 
-struct ArrayExpression : Expression
+class ArrayExpression : public Expression
 {
-	std::string id;
-	Expression index;
+public:
+	explicit ArrayExpression(Type type, std::vector<ExpressionPtr> elements)
+		: Expression(std::move(type))
+		, m_elements(std::move(elements))
+	{
+	}
+
+	void Generate(CodeGenerator &generator) const override
+	{
+		generator.AddInstruction("create_arr " + m_elements.size());
+	}
+
+private:
+	std::vector<ExpressionPtr> m_elements;
 };
